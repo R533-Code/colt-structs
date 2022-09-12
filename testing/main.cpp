813980e93d9493baf492ec967a160e8c70e1ce91
R533-Code <raphaeldibnehmepc@gmail.com>
2022-09-12 09:40:25 +0300

    Added includes

--- testing/main.cpp
+++ testing/main.cpp
@@ -1,29 +1,35 @@
 #include <cstdio>
+
+#define COLT_USE_IOSTREAMS
+#define COLT_USE_FMT
+#include <colt/allocator.h>
 #include <colt/common.h>
 #include <colt/view.h>
 #include <colt/optional.h>
+#include <colt/unique_ptr.h>
+#include <colt/vector.h>
+#include <colt/expected.h>
+
+//TODO: add {fmt} formatting by checking for FMT_VERSION
 
 using namespace colt;
 
 struct Test
 {
   Test() { fputs("Constructed!\n", stdout); }
-  Test(Test&&) { fputs("Move-Constructed!\n", stdout); }
-  Test(const Test&) { fputs("Move-Constructed!\n", stdout); }
+  Test(Test&&) noexcept { fputs("Move-Constructed!\n", stdout); }
+  Test(const Test&) noexcept { fputs("Copy-Constructed!\n", stdout); }
   ~Test() { fputs("Destructed!\n", stdout); }
 };
 
-Optional<int> getInt()
+using namespace colt::sizes;
+
+void print_no_memory() noexcept
 {
-  return 10;
+  fputs("Not enough memory to perform allocation!", stdout);
 }
 
 int main()
 {
-  auto a = getInt();
-  auto b = a;
-  if (a)
-    printf("a: has value %d\n", a.getValue());
-  else
-    printf("None\n");
+  memory::registerOnNullFn(print_no_memory);
 }
