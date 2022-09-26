dc23405ba11c8d0f95086a43422d0f7acf4a3596
R533-Code <raphaeldibnehmepc@gmail.com>
2022-09-26 20:20:35 +0300

    Added main and includes

---
+++ include/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+
+#define COLT_USE_IOSTREAMS
+#include <colt/View.h>
+#include <colt/Vector.h>
+#include <colt/UniquePtr.h>
+#include <colt/String.h>
+#include <colt/Optional.h>
+#include <colt/Expected.h>
+#include <colt/MultiMap.h>
+
+int main()
+{
+
+}
