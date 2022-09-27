135fbb8835b9cbd486ad0154430f6ab598990188
R533-Code <raphaeldibnehmepc@gmail.com>
2022-09-27 22:35:12 +0300

    Added some map tests

--- include/main.cpp
+++ include/main.cpp
@@ -9,7 +9,17 @@
 #include <colt/Expected.h>
 #include <colt/MultiMap.h>
 
+using namespace colt;
+
 int main()
 {
-
+  Map<char, uint64_t> map = { 100 };
+  map.insert('a', 100);
+  if (auto a = map.find('a'))
+    std::cout << "found a " << a->second << '\n';
+  auto [ptr, iresult] = map.insertOrAssign('b', 1000);
+  if (iresult == InsertionResult::ASSIGNED)
+    std::cout << "a now has a value of " << ptr->second;
+  else
+    std::cout << ptr->second;
 }
