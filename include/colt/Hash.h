19b3197fe2bd3e7c0f3a2270cf907db7e8f2b554
R533-Code <raphaeldibnehmepc@gmail.com>
2022-10-01 17:17:50 +0300

    Fixed some comments

--- include/colt/Hash.h
+++ include/colt/Hash.h
@@ -105,19 +105,19 @@ namespace colt
     }
 
     constexpr uint32_t distribute(uint32_t n) {
-      uint32_t p = 0x55555555ul; // pattern of alternating 0 and 1
-      uint32_t c = 3423571495ul; // random uneven integer constant; 
+      uint32_t p = 0x55555555UL; // pattern of alternating 0 and 1
+      uint32_t c = 3423571495UL; // random uneven integer constant
       return c * xorshift(p * xorshift(n, 16), 16);
     }
 
     constexpr uint64_t distribute(uint64_t n) {
-      uint64_t p = 0x5555555555555555ull; // pattern of alternating 0 and 1
-      uint64_t c = 17316035218449499591ull;// random uneven integer constant; 
+      uint64_t p = 0x5555555555555555ULL; // pattern of alternating 0 and 1
+      uint64_t c = 17316035218449499591ULL;// random uneven integer constant
       return c * xorshift(p * xorshift(n, 32), 32);
     }
 
     template <typename T, typename S>
-    constexpr std::enable_if_t<std::is_unsigned<T>::value, T> rotl(const T n, const S i)
+    constexpr std::enable_if_t<std::is_unsigned_v<T>, T> rotl(const T n, const S i)
     {
       const T m = (std::numeric_limits<T>::digits - 1);
       const T c = i & m;
