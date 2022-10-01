dc9d8f9c9033fe519ca71f8c59aca80e11920700
R533-Code <raphaeldibnehmepc@gmail.com>
2022-10-01 17:47:26 +0300

    Tested Map

--- include/main.cpp
+++ include/main.cpp
@@ -7,19 +7,34 @@
 #include <colt/String.h>
 #include <colt/Optional.h>
 #include <colt/Expected.h>
-#include <colt/MultiMap.h>
+#include <colt/Map.h>
 
 using namespace colt;
 
 int main()
 {
-  Map<char, uint64_t> map = { 100 };
-  map.insert('a', 100);
+  Map<char, uint64_t> map;
+  map.insert('a', 0);
+  map.insert('b', 1);
+  map.insert('c', 2);
   if (auto a = map.find('a'))
-    std::cout << "found a " << a->second << '\n';
-  auto [ptr, iresult] = map.insertOrAssign('b', 1000);
+    std::cout << std::boolalpha << "found a " << a->second << '\n';
+  map.erase('a');
+  map.insert('a', 0);
+  if (map.contains('a'))
+    std::cout << "Contains 'a'" << '\n';
+  if (map.contains('b'))
+    std::cout << "Contains 'b'" << '\n';
+
+  for (auto& i : map)
+  {
+    i.second = 100;
+  }
+  std::cout << map;
+
+  auto [ptr, iresult] = map.insert_or_assign('b', 1000);
   if (iresult == InsertionResult::ASSIGNED)
-    std::cout << "a now has a value of " << ptr->second;
+    std::cout << "b now has a value of " << ptr->second;
   else
     std::cout << ptr->second;
 }
