f778c233937b14a4d15a82e2548acbbe15eb185d
R533-Code <raphaeldibnehmepc@gmail.com>
2022-10-06 20:47:59 +0300

    Added test for FlatList

--- include/main.cpp
+++ include/main.cpp
@@ -8,33 +8,18 @@
 #include <colt/Optional.h>
 #include <colt/Expected.h>
 #include <colt/Map.h>
+#include <colt/List.h>
 
 using namespace colt;
 
 int main()
 {
-  Map<char, uint64_t> map;
-  map.insert('a', 0);
-  map.insert('b', 1);
-  map.insert('c', 2);
-  if (auto a = map.find('a'))
-    std::cout << std::boolalpha << "found a " << a->second << '\n';
-  map.erase('a');
-  map.insert('a', 0);
-  if (map.contains('a'))
-    std::cout << "Contains 'a'" << '\n';
-  if (map.contains('b'))
-    std::cout << "Contains 'b'" << '\n';
+  FlatList<int, 2> hello;
+  hello.push_back(1);
+  hello.push_back(10);
+  hello.push_back(100);
+  hello.push_back(1000);
+  hello.push_back(10000);
 
-  for (auto& i : map)
-  {
-    i.second = 100;
-  }
-  std::cout << map;
-
-  auto [ptr, iresult] = map.insert_or_assign('b', 1000);
-  if (iresult == InsertionResult::ASSIGNED)
-    std::cout << "b now has a value of " << ptr->second;
-  else
-    std::cout << ptr->second;
+  std::cout << hello;
 }
