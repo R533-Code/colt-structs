6390c3f46c05e8c3b0e071955d6fa4ecfa4af99a
R533-Code <raphaeldibnehmepc@gmail.com>
2022-10-07 21:41:46 +0300

    Fixed SFINAE and added hash overload

--- include/colt/UniquePtr.h
+++ include/colt/UniquePtr.h
@@ -6,6 +6,7 @@
 #define HG_COLT_UNIQUE_PTR
 
 #include "details/allocator.h"
+#include "colt/Hash.h"
 
 namespace colt
 {
@@ -60,33 +61,34 @@ namespace colt
       return *this;
     }
     
-    template<typename T2>
+    template<typename T2, typename = std::enable_if_t<std::is_convertible_v<T2*, T*>>>
     /// @brief Move constructor for inheritances
     /// @tparam T2 The type of the block whose ownership to steal
     /// @param blk The block whose ownership to steal
-    constexpr UniquePtr(std::enable_if_t<std::is_convertible_v<T*, T2*>, memory::TypedBlock<T2>> blk) noexcept
+    constexpr UniquePtr(memory::TypedBlock<T2> blk) noexcept
       : blk(blk) {}
     
-    template<typename T2>
+    template<typename T2, typename = std::enable_if_t<std::is_convertible_v<T2*, T*>>>
     /// @brief Move constructor for inheritances
     /// @tparam T2 The type of the UniquePtr whose resources to steal
     /// @param to_move The UniquePtr whose resources to steal
-    constexpr UniquePtr(std::enable_if_t<std::is_convertible_v<T*, T2*>, UniquePtr<T2>> to_move) noexcept
-      : blk(blk) {}
+    constexpr UniquePtr(UniquePtr<T2>&& to_move) noexcept
+      : blk(exchange(to_move.blk, { nullptr, 0 })) {}
     
-    template<typename T2>
+    template<typename T2, typename = std::enable_if_t<std::is_convertible_v<T2*, T*>>>
     /// @brief Move assignment operator for inheritances
     /// @tparam T2 The type of the UniquePtr whose resources to steal
     /// @param to_move The UniquePtr whose resources to steal
     /// @return Self
-    constexpr UniquePtr& operator=(std::enable_if_t<std::is_convertible_v<T*, T2*>, UniquePtr<T2>>&& to_move) noexcept
+    constexpr UniquePtr& operator=(UniquePtr<T2>&& to_move) noexcept
     {
       swap(to_move.blk, blk);
       return *this;
     }
 
     /// @brief Destructor, delete the owned resource
-    ~UniquePtr() noexcept(std::is_nothrow_destructible_v<T>)
+    ~UniquePtr()
+      noexcept(std::is_nothrow_destructible_v<T>)
     {
       if (blk)
         memory::delete_t<T>(blk);
@@ -171,7 +173,16 @@ namespace colt
   /// @return UniquePtr of type T
   UniquePtr<T> make_unique(Args&&... args) noexcept(std::is_nothrow_constructible_v<T>)
   {
-    return memory::new_t<T>(std::forward<Args>(args)...);
+    return memory::new_t<T>(std::forward<Args>(args)...);  
+  }
+
+  template<typename T>
+  size_t hash(const UniquePtr<T>& ptr) noexcept
+  {
+    static_assert(traits::is_hashable_v<T>, "Type of UniquePtr should be hashable!");
+    if (ptr)
+      return hash(*ptr);
+    return 18446744073709548283;
   }
 
 #ifdef COLT_USE_IOSTREAMS
